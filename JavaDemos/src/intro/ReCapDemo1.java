3046a4ebdb0e68e6ded4bd4e731af750f39957c0
Görkem Arslan <arslangorkem@outlook.com>
2022-09-18 17:56:04 +0300

    18.09.2022

--- JavaDemos/src/intro/ReCapDemo1.java
+++ JavaDemos/src/intro/ReCapDemo1.java
@@ -4,11 +4,20 @@ public class ReCapDemo1 {
 
 	public static void main(String[] args) {
 		
-		int sayi1=20;
-		int sayi2=25;
+		int sayi1=10;
+		int sayi2=1;
 		int sayi3=2;
 		
+		int enBuyuk =sayi1;
 		
+		if(enBuyuk<sayi2) {
+			enBuyuk=sayi2;
+		}
+		if(enBuyuk<sayi3) {
+			enBuyuk=sayi3;
+		}
+		
+		System.out.println(enBuyuk);
 
 	}
 
