eb785d29c21154db6cb7f128bbe298266e1794a3
Görkem Arslan <arslangorkem@outlook.com>
2022-09-24 19:17:03 +0300

    24.09.2022

---
+++ OOP1Odev/src/OOP1Odev/Metod2.java
@@ -0,0 +1,36 @@
+package OOP1Odev;
+
+public class Metod2 {
+
+	public static void main(String[] args) {
+
+		System.out.println(topla(3, 6));
+		System.out.println(topla2(1, 2, 3, 4, 5, 6));
+
+	}
+
+	public static void ekle() {
+		System.out.println("Eklendi");
+	}
+
+	public static void sil() {
+		System.out.println("Silindi");
+	}
+
+	public static void guncelle() {
+		System.out.println("Güncellendi");
+	}
+
+	public static int topla(int sayi1, int sayi2) {
+		return sayi1 + sayi2;
+	}
+
+	public static int topla2(int... sayilar) {
+		int toplam = 0;
+		for (int sayi : sayilar) {
+			toplam += sayi;
+		}
+		return toplam;
+	}
+
+}
