5c490c6ef4dba3787fb31150caecc11687218a1e
Görkem Arslan <arslangorkem@outlook.com>
2022-09-25 17:14:28 +0300

    25.09.2022

---
+++ OOP1Odev/src/AbstractClasses/Main.java
@@ -0,0 +1,12 @@
+package AbstractClasses;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GameCalculator gameCalculator = new KidsGameCalculator();
+		
+		gameCalculator.hesapla();
+
+	}
+
+}
